b398b7121776f685c71c4e9c5f6f7b2ef8bb7c97
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-10-14 12:50:42 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -8,4 +8,25 @@ Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
 import math
+def distance(a,b,c,d):
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    e = (a ** 2 - c ** 2)
+    e = math.sqrt(math.sqrt(e ** 2))
+    f = (b ** 2 - d ** 2)
+    f = math.sqrt(math.sqrt(f ** 2))
+    g = math.sqrt(e ** 2 + f ** 2)
+    return g
 
+i = input("The number1 is:")
+j = input("The number2 is:")
+k = input("The number3 is:")
+l = input("The number4 is:")
+
+x = distance(i,j,k,l)
+
+round(x, 3)
+
+print(x)
